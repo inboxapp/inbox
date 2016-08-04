496c341eedd7bfbfa204f28876e7174c78fa261f
Ben Gotow <bengotow@gmail.com>
2016-08-04 11:32:43 -0700

    Revert "Silence socket errors and SSL errors until the 20th time in a row"
    This reverts commit 482f2fe53f31f016098280a4277d825ff6d13c73.

--- inbox/util/concurrency.py
+++ inbox/util/concurrency.py
@@ -2,23 +2,17 @@ import functools
 import random
 
 import gevent
-from backports import ssl
-from gevent import socket
 
 from nylas.logging import get_logger
 from nylas.logging.sentry import log_uncaught_errors
 log = get_logger()
-
 BACKOFF_DELAY = 30  # seconds to wait before retrying after a failure
-
-TRANSIENT_NETWORK_ERRS = (socket.timeout, socket.error, ssl.SSLError)
 
 
 def retry(func, retry_classes=None, fail_classes=None, exc_callback=None,
           backoff_delay=BACKOFF_DELAY):
     """
-    Executes the callable func, retrying on uncaught exceptions matching the
-    class filters.
+    Executes the callable func, retrying on uncaught exceptions.
 
     Arguments
     ---------
@@ -59,7 +53,7 @@ def retry(func, retry_classes=None, fail_classes=None, exc_callback=None,
                 if not should_retry_on(e):
                     raise
                 if exc_callback is not None:
-                    exc_callback(e)
+                    exc_callback()
 
             # Sleep a bit so that we don't poll too quickly and re-encounter
             # the error. Also add a random delay to prevent herding effects.
@@ -71,17 +65,8 @@ def retry(func, retry_classes=None, fail_classes=None, exc_callback=None,
 def retry_with_logging(func, logger=None, retry_classes=None,
                        fail_classes=None, account_id=None, provider=None,
                        backoff_delay=BACKOFF_DELAY):
-    network_errs = [0]
-
-    def callback(e):
-        if isinstance(e, TRANSIENT_NETWORK_ERRS):
-            network_errs[0] += 1
-            if network_errs[0] < 20:
-                return
-        else:
-            network_errs[0] = 0
-        log_uncaught_errors(logger, account_id=account_id, provider=provider,
-                            error_type=type(e).__name__)
+    def callback():
+        log_uncaught_errors(logger, account_id=account_id, provider=provider)
 
     return retry(func, exc_callback=callback, retry_classes=retry_classes,
                  fail_classes=fail_classes, backoff_delay=backoff_delay)()
