6218d1db75779f7699f30efaf55239584d271e39
Nils Reimers <rnils@web.de>
2017-09-22 12:01:04 +0200

    Update nylas-sync-startup.sh

--- nylas-sync-startup.sh
+++ nylas-sync-startup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export REPLACE_VARS='$MYSQL_ENV_MYSQL_USERNAME:$MYSQL_ENV_MYSQL_PASSWORD:$MYSQL_PORT_3306_TCP_PORT'
+export REPLACE_VARS='$MYSQL_ENV_MYSQL_USERNAME:$MYSQL_ENV_MYSQL_PASSWORD:$MYSQL_PORT_3306_TCP_PORT:$MYSQL_HOST'
 envsubst "$REPLACE_VARS" < /etc/inboxapp/config-env.json > /etc/inboxapp/config.json
 envsubst "$REPLACE_VARS" < /etc/inboxapp/secrets-env.yml > /etc/inboxapp/secrets.yml
 
