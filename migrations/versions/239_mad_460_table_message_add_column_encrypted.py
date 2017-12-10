2bc165224dfce628d35c362171ac13fa66c0e5f2
Viktor Kuptsov <viktor.kuptsov@together.com>
2017-12-10 18:00:31 +0200

    MAD-460 - added "is_encrypted" flag for messages

---
+++ migrations/versions/239_mad_460_table_message_add_column_encrypted.py
@@ -0,0 +1,26 @@
+"""MAD-460 - table Message - add column 'encrypted'
+
+Revision ID: 731019fd024
+Revises: 369c7cfff3a6
+Create Date: 2017-12-10 15:50:12.286623
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '731019fd024'
+down_revision = '369c7cfff3a6'
+
+from alembic import op
+from sqlalchemy.sql import text
+
+
+def upgrade():
+    conn = op.get_bind()
+    conn.execute(text("ALTER TABLE message"
+                      " ADD column is_encrypted tinyint(1) DEFAULT 0"))
+
+
+def downgrade():
+    conn = op.get_bind()
+    conn.execute(text("ALTER TABLE message"
+                      " DROP column is_encrypted"))
